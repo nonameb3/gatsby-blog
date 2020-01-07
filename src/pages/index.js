d82791bda668742794c768cbac9e75306a0477ac
waraphon <eepw0211@gmail.com>
2020-01-07 22:57:50 +0700

    add makedown file, transformer-remark library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default ({ data }) => {
+  const { edges, totalCount } = data.allMarkdownRemark
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <h4>Welcome to Waraphon Roonnapi new Gatsby-blog site.</h4>
+      <h4>Now let see my {totalCount} blog.</h4>
+      <div>
+        {edges.map(({ node }) => (
+          <div key={node.id}>
+            <span>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </span>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            date
+            title
+          }
+          excerpt
+        }
+      }
+      totalCount
+    }
+  }
+`
